e737d39af83e12d692c9e807e3a89c593f903525
devfancy <fancy.junyongmoon@gmail.com>
2023-12-06 23:27:55 +0900

    test: 재고가 부족한 상품으로 주문을 생성하려는 경우 예외가 발생하는 단위 테스트 작성

--- src/test/java/sample/cafekiosk/spring/api/service/order/OrderServiceTest.java
+++ src/test/java/sample/cafekiosk/spring/api/service/order/OrderServiceTest.java
@@ -153,35 +153,30 @@ class OrderServiceTest {
                 );
     }
 
-    @DisplayName("중복되는 상품번호 리스트로 주문을 생성할 수 있다.")
+    @DisplayName("재고가 부족한 상품으로 주문을 생성하려는 경우 예외가 발생한다.")
     @Test
-    void createOrderWithDuplicateProductNumbers() {
-        // given
+    void createOrderWithNoStock() {
         LocalDateTime registeredDateTime = LocalDateTime.now();
         // given
-        Product product1 = createProduct(HANDMADE, "001", 1000);
-        Product product2 = createProduct(HANDMADE, "002", 3000);
+        Product product1 = createProduct(BOTTLE, "001", 1000);
+        Product product2 = createProduct(BAKERY, "002", 3000);
         Product product3 = createProduct(HANDMADE, "003", 5000);
         productRepository.saveAll(List.of(product1, product2, product3));
 
-        OrderCreateRequest request = OrderCreateRequest.builder()
-                .productNumbers(List.of("001", "001"))
-                .build();
-        // when
-        OrderResponse orderResponse =  orderService.createOrder(request, registeredDateTime);
+        Stock stock1 = Stock.create("001", 1);
+        Stock stock2 = Stock.create("002", 1);
+        stockRepository.saveAll(List.of(stock1, stock2));
 
-        // then
-        assertThat(orderResponse.getId()).isNotNull();
-        assertThat(orderResponse)
-                .extracting("registeredDateTime", "totalPrice")
-                .contains(registeredDateTime, 2000);
-        assertThat(orderResponse.getProducts()).hasSize(2)
-                .extracting("productNumber", "price")
-                .containsExactlyInAnyOrder(
-                        tuple("001", 1000),
-                        tuple("001", 1000)
-                );
-     }
+
+        OrderCreateRequest request = OrderCreateRequest.builder()
+                .productNumbers(List.of("001", "001", "002", "003"))
+                .build();
+
+        // when & then
+        assertThatThrownBy(() -> orderService.createOrder(request, registeredDateTime))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("재고가 부족한 상품이 있습니다.");
+    }
 
     // builder을 메서드로 뺀 것.
     private Product createProduct(ProductType type, String productNumber, int price) {
