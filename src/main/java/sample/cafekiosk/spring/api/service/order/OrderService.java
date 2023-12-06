65966ff7b23964f44ed99faea788097716c71ad7
devfancy <fancy.junyongmoon@gmail.com>
2023-12-06 23:26:59 +0900

    fix: 중복을 제거하기 위해 HashSet 사용

--- src/main/java/sample/cafekiosk/spring/api/service/order/OrderService.java
+++ src/main/java/sample/cafekiosk/spring/api/service/order/OrderService.java
@@ -13,6 +13,7 @@ import sample.cafekiosk.spring.domain.stock.Stock;
 import sample.cafekiosk.spring.domain.stock.StockRepository;
 
 import java.time.LocalDateTime;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -44,7 +45,7 @@ public class OrderService {
                 .collect(Collectors.groupingBy(p -> p, Collectors.counting()));
 
         // 재고 차감 시도
-        for (String stockProductNumber : stockProductNumbers) {
+        for (String stockProductNumber : new HashSet<>(stockProductNumbers)) {
             Stock stock = stockMap.get(stockProductNumber);
             int quantity = productCountingMap.get(stockProductNumber).intValue();
             if(stock.isQuantityLessThan(quantity)) {
