8aaf72092ecc7d82045bc22707f6b67d5fe850e5
devfancy <fancy.junyongmoon@gmail.com>
2023-12-06 21:09:57 +0900

    test: 중복되는 상품번호 리스트로 주문을 생성하는 테스트 코드 작성
    - 아메리카노 연속 2잔을 시킬 수 있도록 리팩터링(RED + GREEN)

--- src/main/java/sample/cafekiosk/spring/api/service/product/order/OrderService.java
+++ src/main/java/sample/cafekiosk/spring/api/service/product/order/OrderService.java
@@ -11,6 +11,8 @@ import sample.cafekiosk.spring.domain.product.ProductRepository;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -21,8 +23,14 @@ public class OrderService {
     public OrderResponse createOrder(OrderCreateRequest request, LocalDateTime registeredDateTime) {
         List<String> productNumbers = request.getProductNumbers();
         List<Product> products = productRepository.findAllByProductNumberIn(productNumbers);
+        Map<String, Product> productMap = products.stream()
+                .collect(Collectors.toMap(Product::getProductNumber, p -> p));// product -> product.getProductNumber(), p -> p
 
-        Order order =  Order.create(products, registeredDateTime);
+        List<Product> duplicateProducts = productNumbers.stream()
+                .map(productMap::get) // productNumber -> productMap.get(productNumbers)
+                .collect(Collectors.toList());
+
+        Order order =  Order.create(duplicateProducts, registeredDateTime);
         Order saveOrder = orderRepository.save(order);
         return OrderResponse.of(saveOrder);
     }
