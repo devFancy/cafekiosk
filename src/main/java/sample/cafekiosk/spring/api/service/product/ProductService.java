4a3e6255e55b213746e2f0ca7e3c4ad2ce1334f3
devfancy <fancy.junyongmoon@gmail.com>
2023-12-08 00:05:52 +0900

    refactor: Command와 Query를 분리하기 위해 Transactional 설정
    - 최상단에 @Transactional(readOnly = true) 설정하고
    - Command(CUD) 작업에만 @Transactional 을 따로 설정한다.

--- src/main/java/sample/cafekiosk/spring/api/service/product/ProductService.java
+++ src/main/java/sample/cafekiosk/spring/api/service/product/ProductService.java
@@ -2,6 +2,7 @@ package sample.cafekiosk.spring.api.service.product;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import sample.cafekiosk.spring.api.controller.product.dto.request.ProductCreateRequest;
 import sample.cafekiosk.spring.api.service.product.response.ProductResponse;
 import sample.cafekiosk.spring.domain.product.Product;
@@ -11,6 +12,14 @@ import sample.cafekiosk.spring.domain.product.ProductSellingStatus;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * readOnly = true : 읽기전용
+ * CRUD 에서 CUD 동작 X / only Read
+ * JPA : CUD 스냅샷 저장 변경감지 X (성능 향상)
+ *
+ * CQRS - Command / Query -> 분리
+ */
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 @Service
 public class ProductService {
@@ -18,6 +27,7 @@ public class ProductService {
     private final ProductRepository productRepository;
 
     // 동시성 이슈
+    @Transactional
     public ProductResponse createProduct(ProductCreateRequest request) {
         String nextProductNumber =  createNextProductNumber();
 
@@ -27,6 +37,7 @@ public class ProductService {
         return ProductResponse.of(savedProduct);
     }
 
+    @Transactional
     private String createNextProductNumber() {
         String latestProductNumber =  productRepository.findLatestProductNumber();
         // 유효성 검사 추가
