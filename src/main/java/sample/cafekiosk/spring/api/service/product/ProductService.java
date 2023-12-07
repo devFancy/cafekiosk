9bc2a8a7bfb23603d46a7554c5a157b38cf896a3
devfancy <fancy.junyongmoon@gmail.com>
2023-12-07 23:40:24 +0900

    feat: 상품을 등록하는 기능 구현
    - 신규 상품을 등록하는 경우에 대한 테스트 코드 작성
    - 상품이 하나도 없는 경우에 대한 테스트 코드 작성

--- src/main/java/sample/cafekiosk/spring/api/service/product/ProductService.java
+++ src/main/java/sample/cafekiosk/spring/api/service/product/ProductService.java
@@ -2,6 +2,7 @@ package sample.cafekiosk.spring.api.service.product;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import sample.cafekiosk.spring.api.controller.product.dto.request.ProductCreateRequest;
 import sample.cafekiosk.spring.api.service.product.response.ProductResponse;
 import sample.cafekiosk.spring.domain.product.Product;
 import sample.cafekiosk.spring.domain.product.ProductRepository;
@@ -16,6 +17,37 @@ public class ProductService {
 
     private final ProductRepository productRepository;
 
+    // 동시성 이슈
+    public ProductResponse createProduct(ProductCreateRequest request) {
+        String nextProductNumber =  createNextProductNumber();
+
+        Product product = request.toEntity(nextProductNumber);
+        Product savedProduct = productRepository.save(product);
+
+        return ProductResponse.builder()
+                .id(savedProduct.getId())
+                .productNumber(nextProductNumber)
+                .type(request.getType())
+                .sellingStatus(request.getSellingStatus())
+                .name(request.getName())
+                .price(request.getPrice())
+                .build();
+    }
+
+    private String createNextProductNumber() {
+        String latestProductNumber =  productRepository.findLatestProductNumber();
+        // 유효성 검사 추가
+        if(latestProductNumber == null) {
+            return "001";
+        }
+
+        int latestProductNumberInt = Integer.parseInt(latestProductNumber);
+        int nextProductNumberInt = latestProductNumberInt + 1;
+
+        // 9 -> 009, 10 -> 010
+        return String.format("%03d", nextProductNumberInt);
+    }
+
     public List<ProductResponse> getSellingProducts() {
         List<Product> products = productRepository.findAllBySellingStatusIn(ProductSellingStatus.forDisplay());
 
@@ -23,4 +55,5 @@ public class ProductService {
                 .map(product -> ProductResponse.of(product))
                 .collect(Collectors.toList());
     }
+
 }
