88fd7af9736c40aca7a4153525bdb3061398f96d
devfancy <fancy.junyongmoon@gmail.com>
2023-12-08 16:02:53 +0900

    feat: 에러를 잡아주기 위해 ApiControllerAdvice 클래스 추가
    - getDefaultMessage에서 원하는 메시지를 출력하기 위해 ProductCreateRequest 클래스에서 message 입력

--- src/main/java/sample/cafekiosk/spring/api/controller/product/dto/request/ProductCreateRequest.java
+++ src/main/java/sample/cafekiosk/spring/api/controller/product/dto/request/ProductCreateRequest.java
@@ -7,19 +7,28 @@ import sample.cafekiosk.spring.domain.product.Product;
 import sample.cafekiosk.spring.domain.product.ProductSellingStatus;
 import sample.cafekiosk.spring.domain.product.ProductType;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
 @Getter
 @NoArgsConstructor
 public class ProductCreateRequest {
 
-    private String productNumber;
+    @NotNull(message = "상품 타입은 필수입니다.")
     private ProductType type;
+
+    @NotNull(message = "상품 판매상태는 필수입니다.")
     private ProductSellingStatus sellingStatus;
+
+    @NotBlank(message = "상품 이름은 필수 입니다.")
     private String name;
+
+    @Positive(message = "상품 가격은 양수여야 합니다.")
     private int price;
 
     @Builder
-    public ProductCreateRequest(String productNumber, ProductType type, ProductSellingStatus sellingStatus, String name, int price) {
-        this.productNumber = productNumber;
+    public ProductCreateRequest(ProductType type, ProductSellingStatus sellingStatus, String name, int price) {
         this.type = type;
         this.sellingStatus = sellingStatus;
         this.name = name;
